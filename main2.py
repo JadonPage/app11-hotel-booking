fc57a6e293393fa3ffb6e682acc6494d55f1bdc5
jadon <jadontpage@outlook.com>
2023-12-27 01:00:00 -0600

    Added method type examples in separate main2 file

---
+++ main2.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv", dtype={"id": str})
+
+
+class Hotel:
+
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+    @classmethod
+    def get_hotel_count(cls, data):
+        return len(data)
+
+
+
+class Ticket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here is your booking data:
+        
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+    @property
+    def the_customer_name(self):
+        name = self.customer_name.strip()
+        name = name.title()
+        return name
+
+    @staticmethod
+    def convert(amount):
+        return amount * 1.2
